e065765016fe0084211f40426727c7abf517aec6
Sergey Gabdullin <s.gabdullin@gmail.com>
2020-08-08 14:22:01 +0500

    develop: изменения верстки + добавил кнопку удалить

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import './TodoItem.css';
 
 const TodoItem = ({ todo, index }) => {
     return (
-    <li className='d-flex justify-content-start align-item-center'>
+    <li className='d-flex justify-content-start align-items-center mb-2'>
         <strong className='mr-2'>{ index + 1 }</strong>
         <input className='mr-2 my-auto' type='checkbox' checked={ todo.complited } />
         <span>{ todo.text }</span>
+        <button className='delete btn btn-outline-danger ml-auto d-flex'>
+            <span className='m-auto'>&times;</span>
+        </button>
     </li>
     );
 };
