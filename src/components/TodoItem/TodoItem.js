ac4e03620ef9ffbcafea0c2577282e208d5eb752
Sergey Gabdullin <s.gabdullin@gmail.com>
2020-08-08 13:59:22 +0500

    develop: Подключил bootstrap

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const TodoItem = ({ todo, index }) => {
     return (
-    <li>
-        <strong>{ index + 1 }</strong>
-        <input type='checkbox' checked={ todo.complited } />
+    <li className='d-flex justify-content-start align-item-center'>
+        <strong className='mr-2'>{ index + 1 }</strong>
+        <input className='mr-2 my-auto' type='checkbox' checked={ todo.complited } />
         <span>{ todo.text }</span>
     </li>
     );
