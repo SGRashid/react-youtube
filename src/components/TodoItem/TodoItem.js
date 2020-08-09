5067ad8237e24c124980c3a28cfaa1ba702ff1b5
Sergey Gabdullin <s.gabdullin@gmail.com>
2020-08-09 14:09:26 +0500

    develop: Добавил стили для выполненного задания

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -5,12 +5,12 @@ const TodoItem = ({ todo, index, onChange, onDelete }) => {
     return (
     <li className='d-flex justify-content-start align-items-center mb-2'>
         <strong className='mr-2'>{ index + 1 }</strong>
-        <input className='mr-2 my-auto' 
+        <input className='mr-2 my-auto cursor-pointer' 
             type='checkbox'
             checked={ todo.complited }
             onChange={ () => onChange(todo.id) }
         />
-        <span>{ todo.text }</span>
+        <span className={ todo.complited ? 'done' : '' }>{ todo.text }</span>
         <button className='delete btn btn-outline-danger ml-auto d-flex'
             onClick={ () => onDelete(todo.id) }
         >
