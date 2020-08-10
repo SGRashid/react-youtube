1f084608424a7f3b5c20ca12d82194d057da9360
Sergey Gabdullin <s.gabdullin@gmail.com>
2020-08-10 14:19:19 +0500

    develop: добавил собственный хук

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -2,16 +2,29 @@ import React, {useContext, useState} from 'react';
 import './AddTodo.css';
 import Context from '../../context';
 
+function useInputValue(defaultValue = '') {
+    const [value, setValue] = useState(defaultValue);
+
+    return {
+        bind: {
+            value,
+            onChange: event => setValue(event.target.value),
+        },
+        clear: () => setValue('')
+    };
+}
+
 const AddTodo = () => {
 
-    const [value, setValue] = useState('');
+    const input = useInputValue();
+    // const [value, setValue] = useState('');
     const {createTodo} = useContext(Context);
 
     const submitHandler = event => {
         event.preventDefault();
-        if (value.trim()) {
-            createTodo(value);
-            setValue('');
+        if (input.bind.value.trim()) {
+            createTodo(input.bind.value);
+            input.clear();
         }
     };
     
@@ -20,8 +33,7 @@ const AddTodo = () => {
             onSubmit={ event => submitHandler(event) }
         >
             <input className='form-control'
-                value={value}
-                onChange={event => setValue(event.target.value)}
+                {...input.bind}
             />
             <button className='ml-2 btn btn-success'
                 type='submit'
