3b6856c18f05914b8584ab511984e4696f6a4b62
Sergey Gabdullin <s.gabdullin@gmail.com>
2020-08-22 13:22:35 +0500

    develop: добавил обрезку заголовков

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -3,6 +3,10 @@ import './TodoItem.css';
 import '../../context';
 import Context from '../../context';
 
+const maxLength = 25;
+
+const textTrim = text => text.length <= maxLength ? text : text.slice(0, maxLength) + '...';
+
 const TodoItem = ({ todo, index }) => {
     const { deleteTodo, toggleTodo } = useContext(Context);
     return (
@@ -13,7 +17,7 @@ const TodoItem = ({ todo, index }) => {
                 checked={ todo.complited }
                 onChange={ () => toggleTodo(todo.id) }
             />
-            <span className={ todo.complited ? 'done' : '' }>{ todo.text }</span>
+            <span className={ todo.complited ? 'done' : '' }>{ textTrim(todo.text) }</span>
             <button className='delete btn btn-outline-danger ml-auto d-flex'
                 onClick={ () => deleteTodo(todo.id) }
             >
